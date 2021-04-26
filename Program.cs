d84ecfaa114fbc9047f6136e8f71e4c84d2095d1
Precious <oluwabunmipreciousajayi@gmail.com>
2021-04-26 10:53:48 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ARRAYS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
